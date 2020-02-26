1b901509c09575a2b51d2c905da454d42bb3d049
gguito <gui155@hotmail.com>
2020-02-26 16:26:18 -0300

    exception handler

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'd33096092059826be89a34f96eb22664',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://63cfa758296c409dad58ddb8eef7216c@sentry.io/2993700',
+  dsn: process.env.SENTRY_DSN,
 };
